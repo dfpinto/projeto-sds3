d285a7a5f0a5fc944514613248b39739271ea1ff
Dirley Pinto <dirley.figueredo@gmail.com>
2021-09-11 17:33:02 -0300

    Integração DonutChart

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,35 +1,36 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/request';
 
 type ChartData = {
-    labels:string[],
-    series:number[]
+    labels: string[],
+    series: number[]
 }
 
 const DonutChart = () => {
 
-    // FORMA ERRADA
-    let chartData : ChartData = {labels:[], series:[]};
-
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-         .then(response => {
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
                 const data = response.data as SaleSum[];
-                const myLabels = data.map(x=>x.sellerName);
-                const mySeries = data.map(x=>x.amount);
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.amount);
 
-                chartData = {labels:myLabels, series:mySeries};
+                setChartData({ labels: myLabels, series: mySeries });
                 console.log(chartData);
             });
+    }, []);
+
     /*
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
     */
-    
+
     const options = {
         legend: {
             show: true
@@ -37,12 +38,12 @@ const DonutChart = () => {
     }
 
     return (
-        <Chart 
-            options={{...options, labels: chartData.labels}}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
-        >    
+        >
         </Chart>
     );
 }
